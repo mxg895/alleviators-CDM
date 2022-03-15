909defb71154a7789d38e33a783ee40fb2ec1ab1
Michelle Gu <michelle.gu@hotmail.com>
2022-03-15 03:25:06 -0700

    Set up resource table structure  - TODO: change dev.py to set connection to Azure's dev postgresql tbl  - TODO: confirm possible duplicate tags  - TODO: finish creating user & preferences tbls

---
+++ backend/pepita_project/settings/prod.py
@@ -0,0 +1,8 @@
+from pepita_project.settings.base import *
+
+# Override base settings as needed for production environment
+DEBUG = False
+
+### TODO: configure Azure Postgresql server as default db
+
+### TODO: Add production client URL as to CORS_ORIGIN_WHITELIST
