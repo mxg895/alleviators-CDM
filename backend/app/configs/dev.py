200fc1451376b837822faa537e4ff2692073d606
Michelle Gu <michelle.gu@hotmail.com>
2022-03-20 03:45:04 -0700

    Add db to dev & prod config files

--- backend/app/configs/dev.py
+++ backend/app/configs/dev.py
@@ -1,10 +1,9 @@
 import os
 ENV = 'development'
-DEBUG=True
-### TODO: add dev database
-# DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-#     dbuser=os.environ['DBUSER_DEV'],
-#     dbpass=os.environ['DBPASS_DEV'],
-#     dbhost=os.environ['DBHOST_DEV'] + ".postgres.database.azure.com",
-#     dbname=os.environ['DBNAME_DEV']
-# )
+DEBUG = True
+DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.environ['DBUSER_DEV'],
+    dbpass=os.environ['DBPASS_DEV'],
+    dbhost=os.environ['DBHOST_DEV'],
+    dbname=os.environ['DBNAME_DEV']
+)
--- backend/app/configs/prod.py
+++ backend/app/configs/prod.py
@@ -1,8 +1,9 @@
+import os
 ENV = 'production'
-### TODO: add production database
-# DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-#     dbuser=os.environ['DBUSER'],
-#     dbpass=os.environ['DBPASS'],
-#     dbhost=os.environ['DBHOST'] + ".postgres.database.azure.com",
-#     dbname=os.environ['DBNAME']
-# )
+DEBUG = False
+DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.environ['DBUSER'],
+    dbpass=os.environ['DBPASS'],
+    dbhost=os.environ['DBHOST'] + ".postgres.database.azure.com",
+    dbname=os.environ['DBNAME']
+)
